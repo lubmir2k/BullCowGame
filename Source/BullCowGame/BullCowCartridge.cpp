b9fb21c59e50c8d51c7f37aa2e86de10d79b247a
lubmir2k <lubomir.mitana@protonmail.com>
2021-10-20 17:18:13 +0200

    Input evaluation

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,21 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+	PrintLine(TEXT("Guess the 4 letter word!"));
+    PrintLine(TEXT("Press Enter to continue..."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
+	FString HiddenWord = TEXT("cake");
+	if(Input == HiddenWord)
+	{
+		PrintLine(TEXT("You have won!"));
+	}
+	else
+	{
+		PrintLine(TEXT("You have lost!"));
+	}
 }
